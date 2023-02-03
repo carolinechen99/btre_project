6685de883b5e4698ae47a5729aecf677afe7f96c
carolinechen99 <carolinexyc99@gmail.com>
2023-02-03 17:51:05 -0500

    6.Models, Migration&Admin

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Listing
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title') # links to edit page
+    list_filter = ('realtor', ) # filter by realtor
+    list_editable = ('is_published',) # edit is_published from list view
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price') # search by these fields
+    list_per_page = 25 # number of listings per page
+
+admin.site.register(Listing, ListingAdmin)
+
+
